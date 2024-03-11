21a349fb15d4d499ad44f71a1d5b1c4cc73bed52
Pedro Henrique Lopes Vieira <pedro.lopes@tvmorena.com.br>
2024-03-11 08:51:06 -0400

    feature_enviarEmail

--- app.py
+++ app.py
@@ -3,28 +3,34 @@ import openpyxl
 import pyautogui
 import time
 import win32com.client
+from disparo_email import enviar_email
 
 def abrir_atualizar_salvar_fechar_excel(file_path):
     # Verifica se o arquivo existe
-    if not os.path.exists(file_path):
-        print(f"O arquivo {file_path} não existe.")
+    if os.path.exists(file_path):
+        file_name = os.path.basename(file_path)
+        print(f"O arquivo {file_name} foi encontrado.")
+    else:
+        print(f"O arquivo {file_path} não foi encontrado.")
         return
-
+    
     # Abre o arquivo Excel em primeiro plano
     xlapp = win32com.client.Dispatch("Excel.Application")
     xlapp.Visible = True
     xlapp.DisplayAlerts = False
+    print("Abrindo o arquivo Excel...")
     wb = xlapp.Workbooks.Open(file_path)
     time.sleep(4)
 
     # Atualiza o Excel (pressiona a tecla F9)
     print("Atualizando o Excel...")
     pyautogui.press('f9')
-    time.sleep(2)  # Aguarda um momento para a atualização
+    time.sleep(2)
 
     # Salva o arquivo Excel
     print("Salvando as alterações...")
     wb.Save()
+    time.sleep(2)
 
     # Fecha o arquivo Excel
     # print("Fechando o arquivo Excel...")
@@ -33,8 +39,13 @@ def abrir_atualizar_salvar_fechar_excel(file_path):
 if __name__ == "__main__":
     # Caminho para o arquivo Excel
     arquivo_excel = "C:\\Users\\pedro.lopes\\teste.xlsx"
+    nome_arquivo = os.path.basename(arquivo_excel)
 
     # Abrir, atualizar, salvar e fechar o arquivo Excel
     abrir_atualizar_salvar_fechar_excel(arquivo_excel)
-
     print("Planilha atualizada, salva e fechada com sucesso.")
+
+    #Disparo via Email
+    print("Enviando planilha por email...")
+    enviar_email(arquivo_excel, nome_arquivo)
+    
---
+++ disparo_email.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def enviar_email(file_path, file_name):
+    # Configurações do servidor SMTP
+    smtp_server = 'smtp.office365.com'
+    smtp_port = 587
+    smtp_username = 'pedrolopesv@outlook.com'
+    smtp_password = '1973284650@PH'
+
+    # Configurações do e-mail
+    sender_email = 'pedrolopesv@outlook.com'
+    receiver_email = 'pedrolopesv@outlook.com'
+    subject = 'TESTE ENVIO DO EXCEL'
+    body = 'TESTE ENVIO DO EXCEL'
+
+ # Criando a mensagem multipart
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+
+    # Adicionando o corpo do e-mail
+    message.attach(MIMEText(body, 'plain'))
+
+  # Anexando o arquivo Excel
+    with open(file_path, 'rb') as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename= {file_name}')
+        message.attach(part)
+
+    # Iniciando a conexão SMTP e enviando o e-mail
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    print("E-mail enviado com sucesso!")
